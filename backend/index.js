b39710b713b63a6ef682ac3a428dab426c1fe35f
zainsajjad903 <zainsajjad.903@gmail.com>
2025-09-26 04:49:19 -0700

    Updated backend API host to Railway domain

--- backend/index.js
+++ backend/index.js
@@ -8,25 +8,19 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // ✅ Ye sabse pehle hona chahiye
-const whitelist = [
-  "https://i-note-book-seven.vercel.app",
-  "http://localhost:3000",
-  // add other allowed origins here
-];
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // allow requests with no origin (mobile apps, curl, same-origin)
-      if (!origin) return callback(null, true);
-      if (whitelist.indexOf(origin) !== -1) {
-        return callback(null, true);
-      } else {
-        return callback(new Error("Not allowed by CORS"));
-      }
-    },
-    optionsSuccessStatus: 200,
-  })
-);
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // allow all origins temporarily
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 app.options("*", cors());
 // Middleware
